bc4c6b561b6c1607473168f19f468317e283207c
Pau Nadeu Rabat <pau.nadeu@everywan.com>
2020-05-09 23:50:39 +0200

    Progressing...

--- README.md
+++ README.md
@@ -1,5 +1,28 @@
 # bgprin
+
+bgpdumpy Build Requirements
+
+Debian/Ubuntu:
+```
+apt install autoconf libtool automake ca-certificates gcc make python-setuptools python-dev libbz2-dev zlib1g-dev libffi-dev
+```
+
+Mac OS:
+```
+brew install autoconf libtool automake
+```
+
+Required Python packages:
 ```
 pip3 install psutil
 pip3 install inquirer
+pip3 install bgpdumpy 
+```
+
+```
+pip3 install exabgp
+sudo mkfifo /var/run/exabgp.{in,out}
+sudo mkdir /var/run/exabgp
+exabgp --fi > /var/run/exabgp/exabgp.env
+sudo mkdir /etc/exabgp && chmod 777 /etc/exabgp
 ```
--- rin.py
+++ rin.py
@@ -1,12 +1,17 @@
 import os.path
-from configparser import ConfigParser
+import sys
+from itertools import count
+
 import psutil
 import inquirer
 
+from configparser import ConfigParser
+from bgpdumpy import BGPDump, TableDumpV2
+
 configPath = 'config.ini'
+exaConfigPath = 'exabgp.conf'
 
-
-def write(IP, BGP, MRT):
+def WriteRINConf(IP, BGP, MRT):
     Config = ConfigParser()
     Config['rin'] = {}
 
@@ -18,6 +23,17 @@ def write(IP, BGP, MRT):
     with open(configPath, 'w') as configfile:
         Config.write(configfile)
 
+
+def WriteExaConf(IP, BGP):
+    Exaconf = "neighbor {} {{\n\
+    router-id {};\n\
+    local-address {};\n\
+    local-as {};\n\
+    peer-as {};\n\
+}}".format(IP['Remote-IPv4'], IP['Local-IPv4'], IP['Local-IPv4'], BGP['Local-AS'], BGP['Remote-AS'])
+
+    with open(exaConfigPath, 'w') as configfile:
+        configfile.write(Exaconf)
 
 def read():
     Config = ConfigParser()
@@ -44,7 +60,7 @@ def configure(section):
             }
             BGP = {'Local-AS': 0, 'Remote-AS': 0}
             MRT = {'File': ''}
-            write(IP, BGP, MRT)
+            WriteRINConf(IP, BGP, MRT)
 
         else:
             # Inquirer!! :)
@@ -83,7 +99,62 @@ def configure(section):
                   'Remote-IPv4': r['Remote-IPv4'], }
             BGP = {'Local-AS': r['Local-AS'], 'Remote-AS': r['Remote-AS']}
             MRT = {'File': r['file']}
-            write(IP, BGP, MRT)
+            WriteRINConf(IP, BGP, MRT)
+            WriteExaConf(IP, BGP)
 
 
-configure('main')
+def run(section):
+    if section == 'set':
+        # Run run run!
+        mrt_file = getCurrentParam('file')
+        if not mrt_file:
+            print("Don't have configured MRT.")
+            exit()
+
+        if not os.path.isfile(mrt_file):
+            print("Invalid MRT file, {} don't exists.".format(mrt_file))
+            exit()
+        else:
+            print("Reading {}".format(mrt_file))
+            with BGPDump(mrt_file) as bgp:
+                exabgpconf = open("exabgp.conf", "w")
+                print(bgp.count())
+                for entry in bgp:
+                    # entry.body can be either be TableDumpV1 or TableDumpV2
+                    if not isinstance(entry.body, TableDumpV2):
+                        continue  # I expect an MRT v2 table dump file
+
+                    # get a string representation of this prefix
+                    prefix = '%s/%d' % (entry.body.prefix, entry.body.prefixLength)
+                    asPath = "{} {}".format(getCurrentParam('Local-AS'), entry.body.routeEntries[0].attr.asPath)
+                    localPref = entry.body.routeEntries[0].attr.localPref
+
+                    exabgpconf.write(
+                        "route {} next-hop {} local-preference {} as-path {};\n".format(prefix, getCurrentParam("Local-IP"),
+                                                                                        localPref, asPath))
+                    # just print it for demonstration purposes
+                    # print('%s -> %s' % (prefix, '/'.join(originatingASs)))
+            exabgpconf.close()
+
+action = ''
+if len(sys.argv) < 2:
+    print("You should specify action to do")
+    exit()
+else:
+    action = sys.argv[1]
+
+if action == 'configure':
+    if len(sys.argv) < 3:
+        print("You should specify context to configure, i.e main")
+        exit()
+    else:
+        context = sys.argv[2]
+        configure(context)
+
+if action == 'run':
+    if len(sys.argv) < 3:
+        print("You should specify context to configure, i.e set")
+        exit()
+    else:
+        context = sys.argv[2]
+        run(context)
